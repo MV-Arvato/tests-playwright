d2e9582fc582b893a2b45833970cca743ce268f9
Vitols Martins <Martins.Vitols@bertelsmann.de>
2025-05-28 17:59:55 +0300

    Fix CI reports

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-	outputDiroutputDir:'./playwright-report',
+  outputDiroutputDir:'./playwright-report',
 	/* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'github' : 'list',	
+	reporter: process.env.CI ? 'github' : 'html',	
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
