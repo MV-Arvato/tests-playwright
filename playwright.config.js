86f65ec489549932b52c6e7df8387be5f9c6d63a
MV-Arvato <141145551+MV-Arvato@users.noreply.github.com>
2025-05-28 17:13:19 +0200

    Setup GitHub workflow  (#1)

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+  outputDiroutputDir:'./playwright-report',
+	/* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -23,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+	reporter: process.env.CI ? [['github'],['html']] : 'html',	
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
