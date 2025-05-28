4ce597eeed6ab44ae9cd1991c33454cc7b38835b
Vitols Martins <Martins.Vitols@bertelsmann.de>
2025-05-28 17:49:29 +0300

    Add OutputDir to config

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
+	outputDiroutputDir:'playwright-report'
+	/* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
