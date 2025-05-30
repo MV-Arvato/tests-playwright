04e74cafc1816c42bd007909c2c66edda8bfc9ca
Vitols Martins <Martins.Vitols@bertelsmann.de>
2025-05-30 03:19:35 +0300

    add cache

---
+++ tests/exmaple3-broken.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  // Navigate to the page
+  await page.goto('https://example.com');
+
+  // Assert the page title contains "Example"
+  await expect(page).toHaveTitle(/Example3/);
+});
+
